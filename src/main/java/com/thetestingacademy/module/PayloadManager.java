cf47601d9052257c6c14a29b2986112c1403509a
VINOD YADAV <vinodyadav4480@gmail.com>
2025-05-20 04:40:54 +0530

    12-May-2025 API Automation Framework

---
+++ src/main/java/com/thetestingacademy/module/PayloadManager.java
@@ -0,0 +1,15 @@
+package com.thetestingacademy.module;
+
+public class PayloadManager {
+
+
+    //Gson gson;
+   // Faker faker;
+
+    // Convert the Java Object into the JSON String to use as Payload.
+    // Serialization
+    public String createPayloadBookingAsString(){
+    return null;
+    }
+}
+
